e566e6a18afec4d72fd003d94b7bbd7d19b8a6ac
DanielProgrammer64 <103775773+DanielProgrammer64@users.noreply.github.com>
2022-11-15 19:13:41 -0300

    Componentes
    .

---
+++ App.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import {View} from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import GoScreen from './screens/GoScreen';
+import { createAppContainer, createSwitchNavigator} from 'react-navigation'; 
+import{
+AppNavigator
+}
+
+ export default class App extends React.Component{
+  render(){
+    return(
+      <View>
+      <AppContainer/>
+      </View>
+    )
+  }
+}
+
+var AppNavigator = createSwitchNavigator({
+  HomeScreen: HomeScreen,
+  GoScreen: GoScreen,
+})
+
+const AppContainer = createAppContainer();
